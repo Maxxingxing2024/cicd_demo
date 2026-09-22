e825c48d40510dca1183f9690dfb418aa5c70361
Max <995717051@qq.com>
2026-09-22 15:55:36 +0800

    修改了test空行问题

--- test_calc.py
+++ test_calc.py
@@ -4,8 +4,10 @@ from calc import my_min, my_add
 def test_min():
     assert my_min(5, 3) == 2
 
+
 def test_add():
     assert my_add(2, 1) == 3
+
 
 def test_add():
     assert my_add(-1, -3) == -4
